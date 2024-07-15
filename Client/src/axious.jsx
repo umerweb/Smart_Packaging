4ee948079614f2358e6aab53eeb015b83137b05d
umer.web <umer.web2@gmail.com>
2024-07-15 23:57:41 +0500

    jkl

--- Client/src/axious.jsx
+++ Client/src/axious.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Instance = axios.create({
-    baseURL : 'https://smart-packaging.onrender.com',
+    baseURL : 'https://smart-packagings.onrender.com',
     timeout: 5000,
     headers:{
         'Content-Type':'application/json',
